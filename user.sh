e4b4f7e68839f31b0dfa3c829858b021c1ff0038
meghraj8788 <meghraj8788@gmail.com>
2025-12-21 18:52:30 +0000

    push

--- user.sh
+++ user.sh
@@ -23,17 +23,23 @@ echo "$user:$pass" | sudo chpasswd
 
 echo "User $user has been created with the password $pass"
 
+
+
+echo "===========USER Created======"
+
+
+echo "=========USER Details=========="
 #show user details
 cat /etc/passwd  | grep $user
 
+
+echo "=======USER Deletion========="
 #delete user
-
-
 sudo userdel -r $user
 
+echo "==========USER Deleted============"
+
 #show user list after delete
-
-
 cat /etc/passwd  | grep  $user
 
 
